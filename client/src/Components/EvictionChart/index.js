0078de3bee09b49107a4a17ada8d727a2c6012c9
John Sasser <cw.john.sasser@gmail.com>
2020-08-17 12:14:15 -0400

    chart positioned and test-data in repo

--- client/src/Components/EvictionChart/index.js
+++ client/src/Components/EvictionChart/index.js
@@ -1,54 +1,87 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
 import {
-    BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Area,
 } from 'recharts';
-    
+import './style.css';
 
-    
-const EvictionChart = props => {
-    //   static jsfiddleUrl = 'https://jsfiddle.net/alidingling/90v76x08/';
-    const data = [
-        {
-          name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-        },
-        {
-          name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-        },
-        {
-          name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-        },
-        {
-          name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-        },
-        {
-          name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-        },
-        {
-          name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-        },
-        {
-          name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-        },
-      ];
+// const [globalTrigger, setGlobalTrigger] = useState();
+// const [caseData, setCaseData] = useState();
 
-        return (
-          <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 20, right: 30, left: 20, bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-            <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
-          </BarChart>
-        );
-    };
+const EvictionChart = (props) => {
+  //   static jsfiddleUrl = 'https://jsfiddle.net/alidingling/90v76x08/';
+  const data = [
+    {
+      fileDate: 'Page A',
+      open: 4000,
+      closed: 2400,
+      amt: 2400,
+    },
+    {
+      fileDate: 'Page B',
+      open: 3000,
+      closed: 1398,
+      amt: 2210,
+    },
+    {
+      fileDate: 'Page C',
+      open: 2000,
+      closed: 9800,
+      amt: 2290,
+    },
+    {
+      fileDate: 'Page D',
+      open: 2780,
+      closed: 3908,
+      amt: 2000,
+    },
+    {
+      fileDate: 'Page E',
+      open: 1890,
+      closed: 4800,
+      amt: 2181,
+    },
+    {
+      fileDate: 'Page F',
+      open: 2390,
+      closed: 3800,
+      amt: 2500,
+    },
+    {
+      fileDate: 'Page G',
+      open: 3490,
+      closed: 4300,
+      amt: 2100,
+    },
+  ];
+
+  return (
+    <BarChart
+      id="barChart"
+      width={1200}
+      height={750}
+      data={data}
+      style={{
+        margin: '0 auto',
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="fileDate" />
+      <YAxis />
+      <Tooltip />
+      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+      <Legend />
+      <Bar dataKey="open" stackId="a" fill="#8884d8" />
+      <Bar dataKey="closed" stackId="a" fill="#82ca9d" />
+    </BarChart>
+  );
+};
 
 export default EvictionChart;
