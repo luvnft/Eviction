37aebbbb57910b88548cfcd3b09d22c26707a5a6
Erik Woodworth <ewoodworth@atlantaregional.org>
2020-08-21 18:05:06 -0400

    Normalized map

--- src/Components/EvictionMap/index.js
+++ src/Components/EvictionMap/index.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Map as LeafletMap, TileLayer, GeoJSON } from 'react-leaflet';
+import numeral from 'numeral';
 import './style.css';
 
 
 const EvictionMap = props => {
 
     const [tractData, setTractData] = useState();
+    // const [normalizeData, setNormalizeData] = useState();
     const [stats, setStats] = useState();
     const [bins, setBins] = useState();
     const colors = ["#0c3383", "#0a7ab1", "#4a9d96", "#cbc74e",  "#d91e1e"];
 
     console.log(props.geojson);
+    
+
+
 
 
     // Create function to setStats({max: value, min: value, range: value})
     const calcStats = data => {
         // console.log(Object.entries(data))
         const valueArray = Object.values(data);
-        // console.log(valueArray);
+        console.log(valueArray);
         const max = Math.max(...valueArray);
         const min = Math.min(...valueArray);
         setStats({
@@ -37,21 +42,40 @@ const EvictionMap = props => {
         }
 
         setBins(bins);
+        console.log(bins);
     }
 
     const handleData = () => {
         const dataObject = {};
+        const normalizeData = {};
+
+       
+        
         props.data
+            // .filter()
             .map(item =>
                 dataObject[item['tractID']] = dataObject[item['tractID']] ?
-                    dataObject[item['tractID']] + parseFloat(item['Count'])
-                    : parseFloat(item['Count']));
+                    (dataObject[item['tractID']] + parseFloat(item['Count']))
+                    : parseFloat(item['Count'])
+            );
+
+        props.normalizeData.map(item => dataObject[item['GEOID']] && item['RentHHs'] ?
+            dataObject[item['GEOID']] = dataObject[item['GEOID']] * 100 / item['RentHHs']
+            : null
+        );
+
+        props.normalizeData.map(item => normalizeData[item['GEOID']] = item['RentHHs'])
+
 
         calcStats(dataObject);
         setTractData(dataObject);
+        // setNormalizeData(normalizeData);
+        console.log(dataObject);
     }
 
-    useEffect(() => { handleData() }, [])
+    useEffect(() => { handleData() }, []);
+    console.log(stats);
+    console.log(bins);
 
     return (
         <LeafletMap
@@ -74,21 +98,26 @@ const EvictionMap = props => {
                     data={props.geojson}
                     // filter={feature => feature.properties['PLNG_REGIO'] !== 'NON-ARC'}
                     style={feature => {
-
-                        const maxValue = stats.max;
-                        const minValue = stats.min;
+                        
                         const geoid = feature.properties['GEOID'];
-                        const value = tractData[geoid]
-                        const distFromMin = value - minValue;
-                        const range = stats.range;
-                        const binningRatio = distFromMin / range;
-                        const numberOfBins = 5;
-                        const color = value ? colors[Math.floor(binningRatio * numberOfBins)] : 'lightgrey';
+                        // const normalizer = normalizeData[geoid];
+                        const value = tractData[geoid];
+                        let color = null;
+                        bins.map((bin, i) =>
+                            value < bin.top && 
+                            value > bin.bottom ? 
+                                color = colors[i]
+                            : null
+                        );
+
+                        console.log(value);
+                        // console.log(normalizer);
+                        console.log(geoid);
 
                         return ({
                             color: value ? 'black' : null,
                             weight: value ? 1 : 0,
-                            fillColor: value ? color : 'lightgrey',
+                            fillColor: color ? color : 'lightgrey',
                             fillOpacity: value ? .8 : 0
                     })
                 }}
@@ -106,7 +135,7 @@ const EvictionMap = props => {
                 <div id='symbol-column'>
                     {
                         colors
-                            .reverse()    
+                            // .reverse()    
                             .map(color =>
                                 <div className='legend-symbol' style={{backgroundColor: color}}/>
                             )
@@ -119,7 +148,7 @@ const EvictionMap = props => {
                             .reverse()
                             .map(bin =>
                                 <div className='legend-label'>
-                                    {`${bin.bottom} to < ${bin.top}`}
+                                    {`${numeral(bin.bottom).format('0,0.0')} to < ${numeral(bin.top).format('0,0.0')}`}
                                 </div>
                             )
                         : null
