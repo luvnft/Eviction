16e201c3d6334ebecfc2db6aaa55ae65706f34fa
Erik Woodworth <ewoodworth@atlantaregional.org>
2020-12-28 18:42:12 -0500

    Revert to old API until GA Tech APIs is made secure

--- client/src/App.js
+++ client/src/App.js
@@ -59,13 +59,13 @@ const App = () => {
     setDateRange({ start: startDate, end: endDate });
   }
 
-  // const getEvictionData = () =>
-  //   API.getData('./evictionsbytract')
-  //     .then(res => {
-  //       setData(res.data);
-  //       handleDateRange(res.data);
-  //     })
-  //     .catch(err => console.error(err));
+  const getEvictionData = () =>
+    API.getData('./evictionsbytract')
+      .then(res => {
+        setData(res.data);
+        handleDateRange(res.data);
+      })
+      .catch(err => console.error(err));
 
   const getContent = () =>
     API.getData('./content')
@@ -75,52 +75,52 @@ const App = () => {
       })
       .catch(err => console.error(err));
   
-  const getEvictionData = () => {
-    const array = [];
-    API.getData('http://evictions.design.gatech.edu/rest/atlanta_metro_area_tracts?select=id,filedate,tractid,countyfp10,totalfilings')
-    .then(res => {
-      res.data
-      .filter(item => 
-        new Date(item.filedate).getTime() >= 
-        new Date('1/1/2020').getTime()
-      )
-      .forEach(item => array.push({
-          "Filing Date": item.filedate,
-          "tractID": parseInt(item.tractid),
-          "COUNTYFP10": parseInt(item.countyfp10),
-          "Total Filings": parseInt(item.totalfilings)    
-      }));
-      API.getData('http://evictions.design.gatech.edu/rest/fulton_county_cases')
-      .then(res => {
-        const object = {}
-        res.data
-        .filter(item => 
-          new Date(item.filedate).getTime() > 
-          new Date('9/18/2020').getTime()
-        )
-        .forEach(item => 
-          object[item.filedate]
-            ? object[item.filedate] = {
-              ...object[item.filedate],
-              "Total Filings" : object[item.filedate]['Total Filings'] + 1
-            }
-            : object[item.filedate] = {
-              "Filing Date" : item.filedate,
-              "tractID" : 9999999,
-              "COUNTYFP10" : 121,
-              "Total Filings" : 1
-            }
-        );
-        Object.values(object).forEach(item => array.push(item))
-        // console.log(array);
-        setData(array);
-        handleDateRange(array);
+  // const getEvictionData = () => {
+  //   const array = [];
+  //   API.getData('http://evictions.design.gatech.edu/rest/atlanta_metro_area_tracts?select=id,filedate,tractid,countyfp10,totalfilings')
+  //   .then(res => {
+  //     res.data
+  //     .filter(item => 
+  //       new Date(item.filedate).getTime() >= 
+  //       new Date('1/1/2020').getTime()
+  //     )
+  //     .forEach(item => array.push({
+  //         "Filing Date": item.filedate,
+  //         "tractID": parseInt(item.tractid),
+  //         "COUNTYFP10": parseInt(item.countyfp10),
+  //         "Total Filings": parseInt(item.totalfilings)    
+  //     }));
+  //     API.getData('http://evictions.design.gatech.edu/rest/fulton_county_cases')
+  //     .then(res => {
+  //       const object = {}
+  //       res.data
+  //       .filter(item => 
+  //         new Date(item.filedate).getTime() > 
+  //         new Date('9/18/2020').getTime()
+  //       )
+  //       .forEach(item => 
+  //         object[item.filedate]
+  //           ? object[item.filedate] = {
+  //             ...object[item.filedate],
+  //             "Total Filings" : object[item.filedate]['Total Filings'] + 1
+  //           }
+  //           : object[item.filedate] = {
+  //             "Filing Date" : item.filedate,
+  //             "tractID" : 9999999,
+  //             "COUNTYFP10" : 121,
+  //             "Total Filings" : 1
+  //           }
+  //       );
+  //       Object.values(object).forEach(item => array.push(item))
+  //       // console.log(array);
+  //       setData(array);
+  //       handleDateRange(array);
 
-      })
-      .catch(err => console.error(err));
-    })
-    .catch(err => console.error(err));
-  }
+  //     })
+  //     .catch(err => console.error(err));
+  //   })
+  //   .catch(err => console.error(err));
+  // }
 
   const AboutContent = {
     Alert: () => (
