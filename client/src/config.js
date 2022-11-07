b2dfb7a5efa5a3bdf354fee8939d201cd3652b9e
ASMBLG <erik.woodworth@asmblg.io>
2022-11-07 17:53:19 -0600

    reenable Fulton

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,10 @@
 export default {
   countyOptions: [
-    { key: '999', text: '4-County Region', value: 999 },
+    { key: '999', text: '5-County Region', value: 999 },
     { key: '063', text: 'Clayton County', value: 63 },
     { key: '067', text: 'Cobb County', value: 67 },
     { key: '089', text: 'Dekalb County', value: 89 },
-    //{ key: '121', text: 'Fulton County', value: 121 },
+    { key: '121', text: 'Fulton County', value: 121 },
     { key: '135', text: 'Gwinnett County', value: 135 }
   ],
   loaderStyle: {
